2d23cd2913dc7f992c9537e2aa9d480bbfba1ae4
romantkacik <romantkacik@gmail.com>
2023-05-02 20:41:40 +0200

    prueba-01

---
+++ Variables/01-variables.js
@@ -0,0 +1,35 @@
+let product = 'tomates'
+console.log (product);
+
+let greetings = 'hola desde';
+let school = 'pmba';
+let greetingsFromSchool = greetings + ' ' + school;
+
+console.log (greetingsFromSchool);
+
+let myDog = 'checo'.toUpperCase();
+let dogPhrase = `quiero mucho a ${myDog}`;
+console.log (dogPhrase);
+
+let name = 'Roman';
+
+if (name === 'Roman') {
+    let age = 25;
+    console.log (`${name} is ${age} years old`);
+}
+
+const myFavoriteHero = 'Hulk'
+console.log(myFavoriteHero)
+
+const h = 5;
+const y = 10;
+let z = h + y;
+console.log(z)
+
+//------------------------------
+
+
+const character = {name: 'jack Frost', age: 10};
+console.log(character);
+
+
